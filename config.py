86d7e9ce42cd8ec7680911cca70aefce0435d19b
oristides <oristides@gmail.com>
2021-12-24 18:38:38 -0300

    mailing Ffrom title erro

--- config.py
+++ config.py
@@ -4,12 +4,15 @@ with open('credentials.json', encoding='utf-8') as f:
 	config= json.load(f)
 
 
-MAILGUN_DOMAIN= config['MAILGUN_DOMAIN']
-MAILGUN_API_KEY=config['MAILGUN_API_KEY']
-FROM_EMAIL=config['FROM_EMAIL']
 USER=config['USER']
 PASSWORD=config['PASSWORD']
 HOST=config['HOST']
 PORT=config['PORT']
 DATABASE=config['DATABASE']
+
 JWT_SECRET_KEY=config['JWT_SECRET_KEY']
+
+MAILGUN_DOMAIN= config['MAILGUN_DOMAIN']
+MAILGUN_API_KEY=config['MAILGUN_API_KEY']
+FROM_EMAIL=config['FROM_EMAIL']
+FORM_TITLE = config['FORM_TITLE']
